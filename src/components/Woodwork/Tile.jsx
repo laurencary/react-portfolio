bf32f0e1aee7cadc9715207ecea611475f7f2a22
laurencary <armstronglg720@gmail.com>
2023-08-05 15:29:21 -0700

    switch wood working photos to aws

--- src/components/Woodwork/Tile.jsx
+++ src/components/Woodwork/Tile.jsx
@@ -1,29 +1,17 @@
-import DiningTable from '../../images/ww/dining_table_final.jpg'
-import CoffeeTable from '../../images/ww/coffee_table_final.jpg'
-import KitchenWorkspace from '../../images/ww/kitchen_workspace_final.jpg'
-import Bench from '../../images/ww/bench_final.jpeg'
-import Desk from '../../images/ww/desk_final.jpg'
-import TallMonitor from '../../images/ww/tall_monitor_final.jpg'
-import ShortMonitor from '../../images/ww/short_monitor_final.jpg'
-import PhoneAmp from '../../images/ww/phone_amp_final.jpg'
-import Vases from '../../images/ww/vases_final.jpg'
-import CheeseCandles from '../../images/ww/cheese_board_candle_final.jpg'
-import Frames from '../../images/ww/poster_frames_final.jpg'
-
 const Tile = ({project}) => {
     const materials = project.materials.join(" | ")
     const photoDict = {
-        "Dining Table": DiningTable,
-        "Coffee Table": CoffeeTable,
-        "Kitchen Workspace": KitchenWorkspace,
-        "Bench": Bench,
-        "Desk": Desk,
-        "Tall Monitor Stand": TallMonitor,
-        "Short Monitor Stand": ShortMonitor,
-        "Phone Amplifier": PhoneAmp,
-        "Vases": Vases,
-        "Cheese Boards & Candle Holders": CheeseCandles,
-        "Poster Frames": Frames
+        "Dining Table": "https://lgc-portfolio-images.s3.us-west-1.amazonaws.com/ww/dining_table_final.jpg",
+        "Coffee Table": "https://lgc-portfolio-images.s3.us-west-1.amazonaws.com/ww/coffee_table_final.jpg",
+        "Kitchen Workspace": "https://lgc-portfolio-images.s3.us-west-1.amazonaws.com/ww/kitchen_workspace_final.jpg",
+        "Bench": "https://lgc-portfolio-images.s3.us-west-1.amazonaws.com/ww/bench_final.jpeg",
+        "Desk": "https://lgc-portfolio-images.s3.us-west-1.amazonaws.com/ww/desk_final.jpg",
+        "Tall Monitor Stand": "https://lgc-portfolio-images.s3.us-west-1.amazonaws.com/ww/tall_monitor_final.jpg",
+        "Short Monitor Stand": "https://lgc-portfolio-images.s3.us-west-1.amazonaws.com/ww/short_monitor_final.jpg",
+        "Phone Amplifier": "https://lgc-portfolio-images.s3.us-west-1.amazonaws.com/ww/phone_amp_final.jpg",
+        "Vases": "https://lgc-portfolio-images.s3.us-west-1.amazonaws.com/ww/vases_final.jpg",
+        "Cheese Boards & Candle Holders": "https://lgc-portfolio-images.s3.us-west-1.amazonaws.com/ww/cheese_board_candle_final.jpg",
+        "Poster Frames": "https://lgc-portfolio-images.s3.us-west-1.amazonaws.com/ww/poster_frames_final.jpg"
         
     }
 
